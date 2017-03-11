f276d27458a1bf3855250b63e38a986e9c87dd79
kimpeter <winkim7@gmail.com>
2017-03-11 16:54:11 +0900

    뷰 수정해서 템플릿 바꿔보기

--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,38 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# Create your views here.
+
 
 def post_create(request):
- 	return HttpResponse("<h1>Create</h1>")
+	return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request): #retrieve
- 	return HttpResponse("<h1>Detail</h1>")
+	context = {
+		"title": "Detail"
+	}
+	return render(request, "index.html", context)
 
 def post_list(request): #list items
-	return render(request, "index.html", {})
+
+	context = {
+		"title": "List"
+	}
+
+	# if request.user.is_authenticated():
+	# 	context = {
+	# 		"title": "My User List"
+	# 	}
+	# else:
+	# 	context = {
+	# 		"title": "List"
+	# 	}
+	return render(request, "index.html", context)
+
 
 def post_update(request):
- 	return HttpResponse("<h1>Update</h1>")
+	return HttpResponse("<h1>Update</h1>")
+
 
 def post_delete(request):
- 	return HttpResponse("<h1>Delete</h1>")
+	return HttpResponse("<h1>Delete</h1>")
