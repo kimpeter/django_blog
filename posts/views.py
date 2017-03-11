b44fdfc94f71c9728ed97736677ad9d32ebbd302
kimpeter <winkim7@gmail.com>
2017-03-11 16:58:28 +0900

    첫 쿼리셋

--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-
+from .models import Post
 
 def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
@@ -14,8 +14,9 @@ def post_detail(request): #retrieve
 	return render(request, "index.html", context)
 
 def post_list(request): #list items
-
+	queryset = Post.objects.all()
 	context = {
+		"object_list": queryset, 
 		"title": "List"
 	}
 
