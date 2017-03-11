96d25059d20664ca3037353c5766dd4c261ce961
kimpeter <winkim7@gmail.com>
2017-03-11 17:05:29 +0900

    동적 url 만들기 + 디테일뷰 추가 + 404화면

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Post
@@ -7,16 +7,19 @@ from .models import Post
 def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
 
-def post_detail(request): #retrieve
+def post_detail(request, id=None): #retrieve
+	#instance = Post.objects.get(id=1)
+    instance = get_object_or_404(Post, id=id)
 	context = {
-		"title": "Detail"
+		"title": instance.title,
+		"instance": instance,
 	}
-	return render(request, "index.html", context)
+	return render(request, "post_detail.html", context)
 
 def post_list(request): #list items
 	queryset = Post.objects.all()
 	context = {
-		"object_list": queryset, 
+		"object_list": queryset,
 		"title": "List"
 	}
 
