22158e07c34804db2008c59bc1fdbc36a6e43d7c
kimpeter <winkim7@gmail.com>
2017-03-11 18:48:29 +0900

    STATIC 파일 추가하기

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -1,4 +1,4 @@
-"""URL Configuration
+"""trydjango19 URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
 Examples:
@@ -13,7 +13,9 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -21,3 +23,6 @@ urlpatterns = [
     url(r'^posts/', include("posts.urls", namespace='posts')),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
